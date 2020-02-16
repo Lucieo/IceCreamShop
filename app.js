4b02fbed682706495445b460df61e396d53a2d23
LucieO <luciecamilleory@gmail.com>
2020-02-16 12:48:14 +0100

    test mysql db

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-
 const errorController = require('./controllers/error');
+const db = require('./util/database');
 
 const app = express();
 
@@ -12,6 +12,15 @@ app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+
+db.execute('SELECT * FROM PRODUCTS')
+    .then(result=>{
+        console.log(result);
+    })
+    .catch(err=>{
+        console.log(err);
+    });
+
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
